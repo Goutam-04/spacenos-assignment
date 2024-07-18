c5d023aaa98f7fbdd85c6599e3bb3da6898df03c
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-07-18 18:14:08 +0530

    add content to the home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,13 @@ export const Hero = () => {
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
-            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-            Ad-Free YouTube Experience with an Extension
+            <h1 className="text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+            Share Your Moments Easily
+            
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-            It automatically skips ads and speeds them up.<br></br>
-            Its user-friendly, efficient, and completely free.<br></br>
-            Watch the video or go to github repo for installation process.
+            Upload an Image, Get a live link <br/>
+             Share with other, Make life Hassle-Free
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
